24a7d98e06166aa15dacba1c281d7594e432d619
Peege <puaygaik@gmail.com>
2015-01-30 19:29:21 +0800

    Did some changes to the model. Still manually associating

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,7 +1,14 @@
+# notes from Estelle
+# - segregate into different controllers
+# - use the URL to pass persistent data for the round e.g. deck_id. For example: /game/:deck_id/cards/card_id
+# - don't rely too much on sessions[]
+# - the logic could be changed so that for every card displayed, create a guess, save the results, display another card
+
 enable :sessions
+require 'byebug'
 
 get '/' do
-  # @all_posts = Post.order('created_at DESC').all
+
   erb :index
 end
 
@@ -26,20 +33,59 @@ end
 # end
 
 post '/game' do
-  session[:cur_deck] = Deck.where(id: params[:d_id]).first.name
-# puts "DECK NUMBER: #{params[:d_id]}"
-  # #create a round
-  a_round = Round.create(deck_id: params[:d_id], user_id: session[:user_id])
-# puts "ROUND #{a_round}"
-  # #retrieve cards
-  deck_of_cards = Card.where(deck_id: params[:d_id])
-  # #populate guesses (WHY NEED TO FIDDLE WITH GUESSES?)
-  deck_of_cards.each do |card|
-    @round_of_guesses = Guess.create(card_id: card.id)
+  if ! session[:in_round]
+    session[:cur_deck] = Deck.where(id: params[:d_id]).first.name
+  # puts "DECK NUMBER: #{params[:d_id]}"
+    # #create a round
+    a_round = Round.create(deck_id: params[:d_id], user_id: session[:user_id])
+  # puts "ROUND #{a_round}"
+    # retrieve cards
+    deck_of_cards = Card.where(deck_id: params[:d_id])
+    # #populate guesses (WHY NEED TO FIDDLE WITH GUESSES?)
+    deck_of_cards.each do |card|
+      Guess.create(card_id: card.id, round_id: a_round.id, correct: 0, incorrect: 0)
+      # Round.create_guess(card_id: card.id, round_id: a_round.id, correct: 0, incorrect: 0)
+byebug
+    end
+
+
+    # store all the guess ids
+    @round_of_guesses = Guess.where(round_id: a_round.id)
+    avail_guesses = []
+    @round_of_guesses.each do |guess|
+      avail_guesses << guess.id
+    end
+    session[:guesses] = avail_guesses
+    # should this be controlled here?
+    session[:in_round] = true
+
+    redirect to '/game'
+  elsif session[:in_round]
+byebug
+    # randomly select a guess
+    session[:guesses].shuffle!
+    cur_guess_id = session[:guesses][0]
+    # remove the guess from avail_guesses
+    session[:guesses].delete_at(0)
+
+    cur_question = Card.find(Guess.find(cur_guess_id).card_id).question
+    cur_answer   = Card.find(Guess.find(cur_guess_id).card_id).answer
+
+    redirect to "/game/#{cur_guess_id}"
+  else
+    puts "SOMETHING WRONG WITH SESSION[:in_round]"
   end
-puts Guess.count
+  # redirect to '/game/'
+  # erb :game
+end
+
+get "/game/:guess_id" do
+byebug
   erb :game
 end
+
+
+
 
 # post '/new' do
 #   erb :new_post
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,5 +1,4 @@
 class Guess < ActiveRecord::Base
   # Remember to create a migration!
   has_one :card
-  has_one :round
 end
